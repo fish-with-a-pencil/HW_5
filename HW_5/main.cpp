ef44bcee545201eae9e8b2b7e0f04ba9eb3b3ca9
Fish <fishingforart@gmail.com>
2016-11-02 09:32:19 -0400

    First commit.  This may be the wrong file.

---
+++ HW_5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW_5
+//
+//  Created by Jessie Day-Aguero on 11/2/16.
+//  Copyright (c) 2016 Jessie Day-Aguero. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+     // insert code here...
+     std::cout << "Hello, World!\n";
+    return 0;
+}
